27cc520ba1b2ee847479b6e2312dac1c86f6e933
KhanyisaSeptember <202224915@ufh.ac.za>
2024-04-10 23:36:00 +0200

    created springboot with a method that returns undergraduate

---
+++ src/main/java/com/example/springbootapplication/Application.java
@@ -0,0 +1,31 @@
+package com.example.springbootapplication;
+
+import org.springframework.boot.SpringApplication ;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/foundation")
+    public Resource foundation() throws IOException {
+        Path path = Paths.get(new ClassPathResource("static/foundationCourses.html").getURI());
+        return new ClassPathResource("static/foundationCourses.html");
+    }
+    @GetMapping("/undergraduate")
+    public Resource undergraduate() throws IOException {
+        Path path = Paths.get(new ClassPathResource("static/undergraduateCourses.html").getURI());
+        return new ClassPathResource("static/undergraduateCourses.html");
+    }
+}
