e01119b756ba5c14661c00db04a343365cce0cec
Aphelele Manjilo <166002917+Aphelele-24@users.noreply.github.com>
2024-04-12 00:17:35 +0200

    Update Application.java
    removed unused imports and variables

--- src/main/java/com/example/springbootapplication/Application.java
+++ src/main/java/com/example/springbootapplication/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -20,17 +20,17 @@ public class Application {
     }
     @GetMapping("/foundation")
     public Resource foundation() throws IOException {
-        Path path = Paths.get(new ClassPathResource("static/foundationCourses.html").getURI());
+        Paths.get(new ClassPathResource("static/foundationCourses.html").getURI());
         return new ClassPathResource("static/foundationCourses.html");
     }
    @GetMapping("/undergraduate")
     public Resource undergraduate() throws IOException {
-        Path path = Paths.get(new ClassPathResource("static/undergraduateCourses.html").getURI());
+         Paths.get(new ClassPathResource("static/undergraduateCourses.html").getURI());
         return new ClassPathResource("static/undergraduateCourses.html");
     }
     @GetMapping("/honours")
     public Resource honours() throws IOException {
-        Path path = Paths.get(new ClassPathResource("static/honoursCourses.html").getURI());
+        Paths.get(new ClassPathResource("static/honoursCourses.html").getURI());
         return new ClassPathResource("static/honoursCourses.html");
     }
 
